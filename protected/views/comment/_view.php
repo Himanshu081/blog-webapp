afca391ebf0df5ec4db1fb5446346e709af79956
himanshu@metricstab.com <himanshu@metricstab.com>
2021-02-18 03:58:59 +0530

    first commit

---
+++ protected/views/comment/_view.php
@@ -0,0 +1,65 @@
+<?php
+/* @var $this CommentController */
+/* @var $data Comment */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+	
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('content')); ?>:</b>
+	<?php echo CHtml::encode($data->content); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('status')); ?>:</b>
+	<?php echo CHtml::encode($data->status); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('create_time')); ?>:</b>
+	<?php echo CHtml::encode($data->create_time); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('author')); ?>:</b>
+	<?php echo CHtml::encode($data->author); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
+	<?php echo CHtml::encode($data->email); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('url')); ?>:</b>
+	<?php echo CHtml::encode($data->url); ?>
+	<br />
+	<b><?php echo CHtml::encode($data->getAttributeLabel('likes')); ?>:</b>
+	<?php echo CHtml::encode($data->likes); ?>
+	<br />
+
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('post_id')); ?>:</b>
+	<?php echo CHtml::encode($data->post_id); ?>	
+	<br />
+	<div class="time">
+		<?php if($data->status==Comment::STATUS_PENDING): ?>
+			<span class="pending">Pending approval</span> |
+			<?php echo CHtml::linkButton('Approve', array(
+				'submit'=>array('comment/like','id'=>$data->id),
+			)); ?> |
+		<?php endif; ?>
+		<?php echo CHtml::link('Like',array('comment/like','id'=>$data->id)); ?>
+
+		<!-- <?php echo CHtml::link('Update',array('comment/update','id'=>$data->id)); ?> |
+		<?php echo CHtml::link('Delete',array('comment/delete','id'=>$data->id),array('class'=>'delete')); ?> | -->
+		<?php echo date('F j, Y \a\t h:i a',$data->create_time); ?>
+	</div>	
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('post_id')); ?>:</b>
+	<?php echo CHtml::encode($data->post_id); ?>
+	<br />
+
+	*/ ?>
+
+</div>
